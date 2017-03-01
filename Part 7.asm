5228c997a3d7b18c4a26f4017ee7a38626b64b1f
Joseph Lyons <josephlyons@josephs-air.pk5001z>
2017-03-01 02:42:44 -0500

    Added comments to guide me along.

--- Part 7.asm
+++ Part 7.asm
@@ -5,5 +5,24 @@
 .text
 .globl main
 
-main:
+#for (i = 0; i < n ‐ 1 ; i ++)
+#{
+#    for (j = 0; j < n – i ‐1; j ++)
+#    {
+#        If (array[j] > array[j+1]
+#        {
+#           temp = array[j];
+#           array[j] = array[j+1];
+#           array[j+1] = temp;
+#        }
+#    }
+#}
 
+# Variable Key:
+# $s0 = n
+# $s1 = i
+# #s2 = j
+# #s3 = array base address
+
+main: 
+      
