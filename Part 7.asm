00cbb925c236f4e5604ba5ab626530a4c36d2446
Joseph Lyons <josephlyons@josephs-air.pk5001z>
2017-03-01 02:57:06 -0500

    Have temp values set up to test program

--- Part 7.asm
+++ Part 7.asm
@@ -5,24 +5,44 @@
 .text
 .globl main
 
-#for (i = 0; i < n ‐ 1 ; i ++)
-#{
-#    for (j = 0; j < n – i ‐1; j ++)
-#    {
-#        If (array[j] > array[j+1]
-#        {
-#           temp = array[j];
-#           array[j] = array[j+1];
-#           array[j+1] = temp;
-#        }
-#    }
-#}
+# for (i = 0; i < n ‐ 1 ; i ++)
+# {
+#     for (j = 0; j < n – i ‐1; j ++)
+#     {
+#         If (array[j] > array[j+1]
+#         {
+#            temp = array[j];
+#            array[j] = array[j+1];
+#            array[j+1] = temp;
+#         }
+#     }
+# }
 
 # Variable Key:
 # $s0 = n
 # $s1 = i
-# #s2 = j
-# #s3 = array base address
+# $s2 = j
+# $s3 = array base address
+# $t0 = n - 1
+# $t1 = 0 or 1 in true false statements (slt)
 
 main: 
+      # Temp values to test program
+      addi $t9, $zero, 15 # Random numbers are being stored in sequential memory
+      addi $t0, $s0, 2
+      sw   $t0, 0($sp)
+      addi $t0, $s0, 2
+      sw   $t0, -4($sp)
+      addi $t0, $s0, -19
+      sw   $t0, -8($sp)
+      la   $t3, 0($sp)
+
+
       
+      # Program actually starts here
+      add  $s1, $zero, $zero # Initialize i to zero
+      addi $t0, $s0,   -1    # $t0 = n - 1
+      slt  $t1, $s1,   $t0   # i < n - 1
+      beq  $t1, $zero, Exit
+      
+Exit:
