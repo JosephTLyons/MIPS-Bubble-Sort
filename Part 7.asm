720f6ebf87224f89d7b0c783b8f4d9104b768bb4
Joseph Lyons <josephlyons@josephs-air.pk5001z>
2017-03-01 03:40:16 -0500

    Inner loop implemented, but its missing the swapping code

--- Part 7.asm
+++ Part 7.asm
@@ -24,29 +24,41 @@
 # $s2 = j
 # $s3 = array base address
 # $t0 = n - 1
-# $t1 = 0 or 1 in true false statements (slt)
+# $t1 = 0 or 1 in true false statements (slt) - outer loop
+# $t2 = $t0 - 1 (n - i - 1)
+# $t3 = 0 or 1 in true false statements (slt) - inner loop
 
 main: 
       # Temp values to test program
-        addi $s0, $zero, 3
+        addi $s0, $zero, 4
         addi $t9, $zero, 15 # Random numbers are being stored in sequential memory
-        addi $t0, $s0, 2
-        sw   $t0, 0($sp)
-        addi $t0, $s0, 2
-        sw   $t0, -4($sp)
-        addi $t0, $s0, -19
-        sw   $t0, -8($sp)
-        la   $t3, 0($sp)
+        addi $t9, $s0, 2
+        sw   $t9, 0($sp)
+        addi $t9, $s0, 2
+        sw   $t9, -4($sp)
+        addi $t9, $s0, -19
+        sw   $t9, -8($sp)
+        addi $t9, $s0, -23
+        sw   $t9, -12($sp)
+        la   $t9, 0($sp)
 
         # Program actually starts here
         add  $s1, $zero, $zero # Initialize i to zero
         addi $t0, $s0,   -1    # $t0 = n - 1
- Outer: slt  $t1, $s1,   $t0   # i < n - 1
+Outer:  slt  $t1, $s1,   $t0   # i < n - 1
         beq  $t1, $zero, Exit  # Exit when i >= n - 1
         
+        add  $s2, $zero, $zero # Initialize i to zero
+        sub  $t2, $t0,   $s1   # $t2 = (n - i - 1)
+Inner:  slt  $t3, $s2,   $t2   # j < (n - i - 1)
+        beq  $t3, $zero, Leave # Exit when j >= (n - i - 1)
         
+ 
+
+        addi $s2, $s2, 1      # Increment j
+        j Inner               
         
-        addi $s1, $s1, 1      # Increment i
+ Leave: addi $s1, $s1, 1      # Increment i
         j Outer
       
 Exit:
