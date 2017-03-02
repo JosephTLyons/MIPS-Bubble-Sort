a6539299feb9a10d1e4d66f71c6bf92bfa492a0d
Joseph Lyons <josephlyons@Josephs-Air.PK5001Z>
2017-03-01 22:12:55 -0500

    Added file documentation and more comments

--- Part 7.asm
+++ Part 7.asm
@@ -1,3 +1,8 @@
+# Title: Homework 4 part 7 - bonus		    #Filename: Part7.asm
+# Author: Joseph Lyons				    #Date: March 1st, 2017
+# Description: Bubble sort
+# Input: None
+# Output: None
 ################# Data segment #####################
 .data
 
@@ -31,21 +36,23 @@
 
 main: 
         # Loading values to test bubble sort
-        addi $s0, $zero, 4  # N numbers in array
-        addi $t9, $zero, 15 # Random numbers are being stored in sequential memory
-        addi $sp, $sp, -16
+        addi $s0, $zero, 5  # n numbers in array
+        addi $t1, $zero, 78 # Random numbers are being stored in sequential memory
+        addi $sp, $sp, -20
         
-        addi $t9, $s0, -5
-        sw   $t9, 12($sp)
-        addi $t9, $s0, -66
-        sw   $t9, 8($sp)
-        addi $t9, $s0, -9
-        sw   $t9, 4($sp)
-        addi $t9, $s0, -23
-        sw   $t9, 0($sp)
+        addi $t1, $s0, -44
+        sw   $t1, 16($sp)
+        addi $t1, $s0, 233
+        sw   $t1, 12($sp)
+        addi $t1, $s0, -9
+        sw   $t1, 8($sp)
+        addi $t1, $s0, -23
+        sw   $t1, 4($sp)
+        addi $t1, $s0, 45
+        sw   $t1, 0($sp)
         add  $s3, $sp, $zero   # Load base address into $s3
 
-        # Program actually starts here
+        # Bubble sort program starts here
         add  $s1, $zero, $zero # Initialize i to zero
         addi $t0, $s0,   -1    # $t0 = n - 1
 Outer:  slt  $t1, $s1,   $t0   # i < n - 1
@@ -58,22 +65,20 @@ Inner:  slt  $t3, $s2,   $t2   # j < (n - i - 1)
         
         sll  $t4, $s2, 2       # j * 4
         
-        add  $t5, $s3, $t4
+        add  $t5, $s3, $t4     # Compare array[j] > array[j + 1]
         lw   $t6, 0($t5)
         lw   $t7, 4($t5)
         slt  $t8, $t7, $t6
         beq  $t8, $zero, Skip
         
-        #swap
-        add  $t9, $t6, $zero
+        add  $t9, $t6, $zero   # Perform swap
         sw   $t7, 0($t5)
         sw   $t9, 4($t5)
- 
 
-Skip:   addi $s2, $s2, 1       # Increment j
+Skip:   addi $s2, $s2, 1       # Increment j for inside loop
         j Inner               
         
- Leave: addi $s1, $s1, 1       # Increment i
+Leave:  addi $s1, $s1, 1       # Increment i for outside loop
         j Outer
       
 Exit:
